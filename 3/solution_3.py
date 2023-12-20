32de9d5f290dc23db2f2b748df8cf2e0c489d3eb
forguesma21 <marylieforgues3@gmail.com>
2023-12-20 13:13:05 -0500

    day 4: solution

--- 3/solution_3.py
+++ 3/solution_3.py
@@ -14,9 +14,27 @@ test_engine = '''467..114..
 ...$.*....
 .664.598..'''
 
-board = list(test_engine)
-print(board)
+board = list(open("3\input_3.txt"))
+symbols_location = {(r, c): [] for r in range(140) for c in range(140)
+                    if board[r][c] not in '01234566789.'}
 
-board = list(open('input_3.txt'))
+print(chars)
+
+
+# User
+
+'''board = list(open('data.txt'))
 chars = {(r, c): [] for r in range(140) for c in range(140)
          if board[r][c] not in '01234566789.'}
+
+for r, row in enumerate(board):
+    for n in re.finditer(r'\d+', row):
+        edge = {(r, c) for r in (r-1, r, r+1)
+                for c in range(n.start()-1, n.end()+1)}
+
+        for o in edge & chars.keys():
+            chars[o].append(int(n.group()))
+
+print(sum(sum(p) for p in chars.values()),
+      sum(m.prod(p) for p in chars.values() if len(p) == 2))
+expliquer'''
